1f5ddc62dca40bddecabdd1682a53ab5597cad24
Leo Li <xiaohu.li@utas.edu.au>
2025-07-23 13:28:15 +1000

    Uncomment module configuration for poc-imos-wis2-2a-test in main.tf

--- wis2-terraform/main.tf
+++ wis2-terraform/main.tf
@@ -3,7 +3,7 @@ module "poc-imos-wis2-2a" {
   instance_name = "imos-wis2-2a"
 }
 
-# module "poc-imos-wis2-2a-test" {
-#   source = "./ec2"
-#   instance_name = "poc-imos-wis2-2a-test"
-# }
+module "poc-imos-wis2-2a-test" {
+  source = "./ec2"
+  instance_name = "imos-wis2-2a-test"
+}
